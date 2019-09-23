109cff07cb6a41242515f4ebcfb4c04e5ebc8c18
Oliver Wissett <ow23@st-andrews.ac.uk>
2019-09-23 14:05:48 +0100

    week-2

--- Week-2/main.r
+++ Week-2/main.r
@@ -5,24 +5,73 @@
 ## Nice little function that I found that will install packages if they are not 
 ## installed.
 
-ipak <- function(pkg){
-  
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  
-  if (length(new.pkg)) 
+{
+  ipak <- function(pkg){
     
-    install.packages(new.pkg, dependencies = TRUE)
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    
+    if (length(new.pkg)) 
+      
+      install.packages(new.pkg, dependencies = TRUE)
+    
+    sapply(pkg, require, character.only = TRUE)
+    
+  }
   
-  sapply(pkg, require, character.only = TRUE)
+  libs <- c("ggplot2", 
+            "gridExtra",
+            "itsmr",
+            "pracma",
+            "git2r",
+            "car",
+            "minpack.lm",
+            "ggthemes",
+            "Hmisc")
   
+  ipak(libs) ## Installs and loads all packages listed in "libs".
+  libs <- NULL ##Clear the memory.
 }
 
-libs <- c("ggplot2", 
-          "gridExtra",
-          "itsmr",
-          "pracma",
-          "git2r",
-          "car",
-          "minpack.lm")
+######################
+##    Load Data     ##
+######################
 
-ipak(libs) ## Installs and loads all packages listed in "libs".
+dat1 <- read.csv("data1.csv", header = T, sep = ",")
+dat2 <- read.csv("data2.csv", header = T, sep = ",")
+
+######################
+##  Data Formating  ##
+######################
+
+dat1$Shoe.Size <- factor(dat1$Shoe.Size)
+
+dat1Monday <- subset(dat1, Lecture == "Monday")
+dat1Tuesday <- subset(dat1, Lecture == "Tuesday")
+dat1Wednesday <- subset(dat1, Lecture == "Wednesday")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
